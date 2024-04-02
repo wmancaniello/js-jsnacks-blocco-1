9114d6b3c7fda04951bcecdcdecfe90704dab415
wmancaniello <wmancaniello@gmail.com>
2024-04-02 18:11:28 +0200

    Snack 6 completed

--- JSnack 6/js/script.js
+++ JSnack 6/js/script.js
@@ -1,2 +1,13 @@
-// test
-alert("Ciao");
+//  n è un numero indicato dall'utente;
+//  Stampa il cubo dei primi n numeri.
+
+//  Chiedo un numero all'utente:
+const userNumber = parseInt(prompt("Dimmi un numero:"));
+
+//  Creo un ciclo che mi calcoli il cubo dei numeri dall'1 al numero inserito dall'utente
+
+for (let i = 1; i <= userNumber; i++) {
+  //  ** equivale all'esponente, è come scrivere i * i * i
+  let cubo = i ** 3;
+  console.log(`Il cubo di: ${i} è = ${cubo}`);
+}
