723296a347af164d3b5aa0b3818b78c5b2187aa9
wmancaniello <wmancaniello@gmail.com>
2024-04-03 11:33:23 +0200

    fix

--- JSnack 4/js/script.js
+++ JSnack 4/js/script.js
@@ -2,8 +2,46 @@
 // Chiedi all'utente il suo nome;
 // Comunicagli se può partecipare o no alla festa.
 
+// Metodo for
+
 // Array degli invitati
+// const partyGuests = [
+//   "Jay",
+//   "Nick",
+//   "Daisy",
+//   "Tom",
+//   "Jordan",
+//   "Myrtle",
+//   "George",
+//   "Meyer",
+// ];
+
+// Chiedo il nome all'utente tramite prompt
+// const userName = prompt("Inserisci il tuo nome:");
+// console.log(userName);
+
+//  Inserisco una variabile d'appoggio false
+// let guest = false;
+
+// Controllo se il nome dell'utente è presente nell'array degli invitati
+// for (let i = 0; i < partyGuests.length; i++) {
+//     if (partyGuests[i] === userName) {
+//         guest = true;
+//         break;
+//     }
+// }
+
+//  Stampo il risultato, quindi se può partecipare o no alla festa
+// if (guest) {
+//     console.log("Puoi partecipare alla festa!");
+// } else {
+//     console.log("Mi dispiace, ma non puoi partecipare alla festa :(");
+// }
+
+//  Metodo while
+
 const partyGuests = [
+  //  array
   "Jay",
   "Nick",
   "Daisy",
@@ -14,24 +52,31 @@ const partyGuests = [
   "Meyer",
 ];
 
-// Chiedo il nome all'utente tramite prompt
-const userName = prompt("Inserisci il tuo nome:");
-console.log(userName);
+//Input
+//  Chiedere all'utente il suo nome
+const userName = prompt("Dimmi il tuo nome"); //  string
 
-//  Inserisco una variabile d'appoggio false
-let guest = false;
+//  Per ogni elemento dell'array
+//      Per ogni elemento dell'array
+//      Se l'elemento è uguale al nome utente
+//          isFound è uguale a true
+//      Incrementare l'indice dell'array
 
-// Controllo se il nome dell'utente è presente nell'array degli invitati
-for (let i = 0; i < partyGuests.length; i++) {
-    if (partyGuests[i] === userName) {
-        guest = true;
-        break;
-    }
+let i = 0;
+let isFound = false;
+while (i < partyGuests.length && !isFound) {
+  const curGuest = partyGuests[i];
+  //   console.log(curGuest);
+
+  if (curGuest === userName) {
+    isFound = true;
+  }
+
+  i++;
 }
 
-//  Stampo il risultato, quindi se può partecipare o no alla festa
-if (guest) {
-    console.log("Puoi partecipare alla festa!");
+if (isFound) {
+  console.log("Puoi entrare!");
 } else {
-    console.log("Mi dispiace, ma non puoi partecipare alla festa :(");
+  console.log("Mi dispiace ma non puoi entrare.");
 }
